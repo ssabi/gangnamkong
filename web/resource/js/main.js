154a2c0b24b3874470f74b6f0ac6d2491b7c9276
ssabi <ensemble1224@gmail.com>
2021-04-10 01:10:08 +0900

    PC메인 작업

--- web/resource/js/main.js
+++ web/resource/js/main.js
@@ -1,8 +1,7 @@
-
 $( document ).ready(function() {
 	//Main Visual Swiper
 	var visualSwiper = new Swiper('.visual .swiper-container', {
-				loop: true,
+		loop: true,
 		autoplay: {
 			delay: 5000,
 			disableOnInteraction: false,
@@ -15,31 +14,31 @@ $( document ).ready(function() {
 			nextEl: '.swiper-button-next',
 			prevEl: '.swiper-button-prev',
 		},
-				on:{
-						transitionEnd:function(s){
-								//s.updateSize();
-								//s.updateSlides();
-								//s.updateProgress();
-								s.update();
-						},
-						transitionStart:function(s){
-								//s.updateSize();
-								//s.updateSlides();
-								//s.updateProgress();
-								s.update();
-						}
-				}
+		on:{
+			transitionEnd:function(s){
+				//s.updateSize();
+				//s.updateSlides();
+				//s.updateProgress();
+				s.update();
+			},
+			transitionStart:function(s){
+				//s.updateSize();
+				//s.updateSlides();
+				//s.updateProgress();
+				s.update();
+			}
+		}
 	});
-		var recipeSwiper = new Swiper('.recipe .swiper-container', {
-				slidesPerView: 'auto',
-				spaceBetween: 30,
-				preventClicks: true,
-				navigation: {
+	var recipeSwiper = new Swiper('.recipe .swiper-container', {
+		slidesPerView: 'auto',
+		spaceBetween: 30,
+		preventClicks: true,
+		navigation: {
 			nextEl: '.swiper-button-next',
 			prevEl: '.swiper-button-prev',
 		},
-				pagination: {
-						el: '.swiper-pagination',
-				}
-		});
+		pagination: {
+			el: '.swiper-pagination',
+		}
+	});
 });
