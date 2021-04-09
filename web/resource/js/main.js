303d5b24924670b3417164eadea6e1a08b92f140
ssabi <ensemble1224@gmail.com>
2021-04-10 01:08:54 +0900

    PC메인 작업

--- web/resource/js/main.js
+++ web/resource/js/main.js
@@ -1,22 +1,45 @@
 
 $( document ).ready(function() {
-  //banner
-  var bannerSwiper = new Swiper('.banner .swiper-container', {
-    centeredSlides: true,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    }
-  });
-  var newSwiper = new Swiper('.new-product .swiper-container', {
-    slidesPerView: 'auto',
-    //centeredSlides: true,
-    spaceBetween: 30,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    //watchSlidesProgress: true,
-    watchSlidesVisibility: true,
-  });
+	//Main Visual Swiper
+	var visualSwiper = new Swiper('.visual .swiper-container', {
+				loop: true,
+		autoplay: {
+			delay: 5000,
+			disableOnInteraction: false,
+		}
+	});
+	var newSwiper = new Swiper('.new-product .swiper-container', {
+		slidesPerView: 'auto',
+		//spaceBetween: 30,
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+				on:{
+						transitionEnd:function(s){
+								//s.updateSize();
+								//s.updateSlides();
+								//s.updateProgress();
+								s.update();
+						},
+						transitionStart:function(s){
+								//s.updateSize();
+								//s.updateSlides();
+								//s.updateProgress();
+								s.update();
+						}
+				}
+	});
+		var recipeSwiper = new Swiper('.recipe .swiper-container', {
+				slidesPerView: 'auto',
+				spaceBetween: 30,
+				preventClicks: true,
+				navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+				pagination: {
+						el: '.swiper-pagination',
+				}
+		});
 });
