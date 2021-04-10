8af71990c6f2371848b6aa47ea7892c1b282146d
ssabi <ensemble1224@gmail.com>
2021-04-10 15:58:01 +0900

    상품상세페이지

--- web/resource/js/content.js
+++ web/resource/js/content.js
@@ -1,7 +1,34 @@
 
-$( document ).ready(function() {
+$(document).ready(function() {
   //찜하기
   $( '.ico-heart' ).click( function() {
     $(this).toggleClass( 'on' );
+  });
+  //tab
+  $(".tab_content").hide(); //Hide all content
+  $("ul.tab-nav li:first").addClass("on").show(); //Activate first tab
+  $(".tab_content:first").show(); //Show first tab content
+
+  //On Click Event
+  $("ul.tab-nav li").click(function () {
+
+    $("ul.tab-nav li").removeClass("on"); //Remove any "on" class
+    $(this).addClass("on"); //Add "on" class to selected tab
+    $(".tab_content").hide(); //Hide all tab content
+
+    var activeTab = $(this).find("a").attr("href"); //Find the href attribute value to identify the active tab + content
+    $(activeTab).fadeIn(); //Fade in the active ID content
+    return false;
+  });
+
+  //관련상품 스와이퍼
+  var RelatedSwiper = new Swiper('.sub-bottom-swiper .swiper-container', {
+    slidesPerView: 4,
+    loop: true,
+    spaceBetween: 30,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
   });
 });
