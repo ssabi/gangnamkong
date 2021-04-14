67326e69d7b01f47e4a06717dafc9b2f4312098b
ssabi <ensemble1224@gmail.com>
2021-04-14 21:21:47 +0900

    장바구니 작업

--- mo/resource/js/ui-common.js
+++ mo/resource/js/ui-common.js
@@ -49,6 +49,7 @@ $(document).ready(function(){
 	});
 
 	//category menu
+	/*
 	$(".sub-menu").hide();
 	$(".category .menu li>a").click(function(){
 		$(this).parent().addClass('on').siblings().removeClass('on');
@@ -59,6 +60,75 @@ $(document).ready(function(){
 		}else{
 				submenu.slideDown();
 				submenu.parent().siblings('li').find('.sub-menu').slideUp();
+		}
+	});
+	*/
+	$(".category .menu > li > a").on({
+		click : function(e){
+			e.preventDefault();
+
+			$(this).parent().toggleClass('on').siblings("li").removeClass("on");
+
+			var dropdown = $(this).next(".sub-menu");
+
+			//$con.not(dropdown).removeClass("show");
+			//dropdown.toggleClass("show");
+			$(".sub-menu").not(dropdown).slideUp(200);
+
+			dropdown.slideToggle(200);
+		}
+	});
+
+	$(".ico_btm_category").on({
+		click : function(e){
+			e.preventDefault();
+			$(this).closest("li").addClass("on");
+			var $target = $(".category-menu-wrap");
+			var $html = $("html");
+			$target.addClass("active");
+			$html.addClass("ovf");
+			
+
+			$(".category-menu-wrap .close-menu").off("click").on({
+				click : function(e){
+					e.preventDefault();
+					$(".ico_btm_category").closest("li").removeClass("on");
+					$(this).closest(".category-menu-wrap").removeClass("active");
+					$html.removeClass("ovf");
+				}
+			});
+		}
+	});
+
+	//수량 선택
+	$(".btn-minus").on({
+		click : function(){
+			var $target = $(this).closest(".btn-quantity").find("input");
+			var $value = $target.val();
+
+			var exp = eval($value) - 1;
+			if(exp <= 0){
+				exp = 0;
+			}
+			$target.val(exp);
+		}
+	});
+	$(".btn-plus").on({
+		click : function(){
+			var $target = $(this).closest(".btn-quantity").find("input");
+			var $value = $target.val();
+
+			var exp = eval($value) + 1;
+			$target.val(exp);
+		}
+	});
+
+	//
+	$(".btn_accord").on({
+		click : function(){
+			$(this).toggleClass("fold");
+			var $target = $(this).closest(".accord_wrap").find(".accord_con");
+			$target.slideToggle(200);
 		}
 	});
 });
